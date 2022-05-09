28149075135ac37315bb6961be62d72c08ae8804
EunBin <jang020410@naver.com>
2022-05-09 15:04:45 +0900

    [10주차] 총알 발사 다시 진행중

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private Text score;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.score = GameObject.Find("Score").GetComponent<Text> ();
+        count = 0;    
+    }
+
+    public void Incscore()
+    {
+        count+=1;
+        this.score.text = count.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
